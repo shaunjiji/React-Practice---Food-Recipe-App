03ae8d0a1d0054ea1689ecfd0a60af31f3185460
Shaun Jiji <shaunjiji123@gmail.com>
2023-01-23 13:39:23 +0530

    introduce recipe state into app.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import { useState } from "react";
 
 function App() {
   const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
   var url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=8849044a&app_key=bb9052a8d9d5b7ad3d63b276a7402702
   `;
 
   async function getRecipes() {
     var result = await axios.get(url);
-    console.log(result.data);
+    setRecipes(result.data.hits);
+    console.log(result);
   }
 
-  function submit(e) {
-    e.preventDefault();
-    getRecipes();
-  }
   const onSubmit = (e) => {
     e.preventDefault();
     getRecipes();
@@ -34,6 +33,7 @@ function App() {
         />
         <input className="app__submit" type="submit" value="Search" />
       </form>
+      <div></div>
     </div>
   );
 }
