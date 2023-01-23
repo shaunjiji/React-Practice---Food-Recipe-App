32081ee2169ca04f2173fbab58e3aecb72a313cb
Shaun Jiji <shaunjiji123@gmail.com>
2023-01-23 12:45:29 +0530

    add api url and make test fetch request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  var baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&q='chicken'&app_id=8849044a&app_key=bb9052a8d9d5b7ad3d63b276a7402702
+  `;
+
+  async function getRecipes() {
+    var result = await axios.get(url);
+    console.log(result.data);
+  }
+
   return (
     <div className="App">
       <h1>Hello Shaun</h1>
